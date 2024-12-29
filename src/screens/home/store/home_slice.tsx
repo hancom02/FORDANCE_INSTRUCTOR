98b9cbf04a3567b5c68c785bff0fccff39907951
ngochan <20520484@gm.uit.edu.vn>
2024-12-29 14:16:58 +0700

    feat: add HomeSlice

---
+++ src/screens/home/store/home_slice.tsx
@@ -0,0 +1,63 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
+import { supabase } from '../../../supabase_config/supabase';
+import { EStatus } from '../../../types/status_enum';
+
+export const useInstructorData = create(
+  persist(
+    immer((set) => ({
+    //   isLogin: undefined, 
+    //   email: '',
+      error: undefined,    
+      getClassByInstructor: async (instructorId: string): Promise<Class[] | null> => {
+        try {
+          console.log('Fetching sessions for instructor:', instructorId);
+
+          const { data: classData, error: classError } = await supabase
+            .from('classes')
+            .select('*')
+            .match({ instructor_id: instructorId });      
+          if (classError) {
+            console.error('Error:', classError.message);
+            set((state) => {
+              state.error = classError.message;
+            });            
+            throw new Error(classError.message);
+          }
+          return classData as Class[];
+        } catch (err) {
+          throw new Error(err.message);
+        }
+      },
+      getSessionByInstructor: async (instrutorId: string): Promise<Session[] | null> => {
+        try {
+          console.log('Fetching sessions for instructor:', instrutorId);
+
+          const { data: sessionData, error: sessionError } = await supabase
+            .from('sessions')
+            .select('*')
+            .eq('instructor_id', instrutorId)
+            .neq('status', EStatus.Deleted)
+            .neq('status', EStatus.Rejected);
+            
+          if (sessionError) {
+            console.error('Error:', sessionError.message);
+            set((state) => {
+              state.error = sessionError.message;
+            });
+            throw new Error(sessionError.message);
+          }
+          return sessionData as Session[];
+        } catch (err) {
+          throw new Error(err.message);
+        }
+      },
+    })),
+    {
+      name: 'instructor-data',
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
