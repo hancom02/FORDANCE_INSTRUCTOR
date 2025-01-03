4f7090a57b6cedbb9e1b88a7f3963c2cc1c8142b
ngochan <20520484@gm.uit.edu.vn>
2025-01-03 09:50:45 +0700

    feat: add color

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -3,7 +3,9 @@ const MyColor = {
     primary: '#CE00FF',
     blue: '#0000ff',
     white: '#ffffff',
+    black: '#000000',
     lightGrey: '#f5f5f5',
+    gray: '#808080',
 }
 
 export default MyColor;
